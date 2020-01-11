36338305b028ac3acd6b5d8746bcf3c82a3c8596
Zhaoyi Shen <pkuszy@gmail.com>
2020-01-11 15:30:28 -0800

    small change in Radiation.pyx

--- Radiation.pyx
+++ Radiation.pyx
@@ -522,7 +522,7 @@ cdef class RadiationRRTM(RadiationBase):
         try:
             self.coszen = namelist['radiation']['RRTM']['coszen']
         except:
-            if (self.toa_sw > 420.0):
+            if (self.toa_sw > 0.0):
                 self.coszen = self.toa_sw / self.scon 
             else:
                 Pa.root_print('Mean Daytime cos(SZA) not set so RadiationRRTM takes default value: coszen = 2.0/pi .')
